d83d1ccd52a50181d6d6b8151655920eaeaf7eb7
Zelinskyi Maksym <zelinskyi.ma@gmail.com>
2026-03-28 16:59:16 +0200

    End task3

--- Var3/Program.cs
+++ Var3/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Var3;
+﻿using Var3;
+
+namespace Var3;
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var container = new DataContainer<DataItem>();
+        for (int i = 0; i < 10000; i++)
+        {
+            var item = new DataItem
+            {
+                Id = $"item-{i}",
+                Payload = new byte[1024]
+            };
+            container.AddItem(item);
+        }
+        Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        Console.WriteLine("GC.Collect() was called.");
+        Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes");
     }
 }
