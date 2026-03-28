489c940dfb2bd73655686d46b5385590e09451b7
Zelinskyi Maksym <zelinskyi.ma@gmail.com>
2026-03-28 16:47:21 +0200

    End Stage2

--- Var3/DataContainer.cs
+++ Var3/DataContainer.cs
@@ -1,6 +1,22 @@
-namespace Var3;
+using System.Collections.Generic;
 
-public class DataContainer
+
+namespace Var3;
+public class DataContainer<T> where T : DataItem
 {
-    
+    private readonly List<T> _items = new();
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+    public long GetTotalSize()
+    {
+        long totalSize = 0;
+        foreach (T item in _items)
+        {
+            totalSize += item.GetSize();
+        }
+
+        return totalSize;
+    }
 }
